4cb77efc68a7f5c6b0980d8e2df006f802b3bdf5
PaoloIncollu <paoloinco@gmail.com>
2024-07-17 17:30:16 +0200

    correzione immagini

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,12 @@
 // creazione e riempimento array
 const imgArray = [
 
-    'file:///C:/Users/paolo/AppData/Local/Temp/ea6cc6d4-8a88-4df3-9fb5-ba84aa7d43a6_consegna.zip.3a6/consegna/img/01.webp',
-    'file:///C:/Users/paolo/AppData/Local/Temp/a3b841d0-528a-4051-958e-5b00c6dfe0f6_consegna.zip.0f6/consegna/img/02.webp',
-    'file:///C:/Users/paolo/AppData/Local/Temp/15d44351-6ef2-4b1c-a8f8-47ce845cd9c4_consegna.zip.9c4/consegna/img/03.webp',
-    'file:///C:/Users/paolo/AppData/Local/Temp/1ab3c0ee-51a0-43ec-a7b0-7d23c4713ca0_consegna.zip.ca0/consegna/img/04.webp',
-    'file:///C:/Users/paolo/AppData/Local/Temp/65381888-cdea-4b3d-90d5-5b655be21dc6_consegna.zip.dc6/consegna/img/05.webp'
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp',
+    
 
 ];
 
